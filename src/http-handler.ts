8169d8f40672b9dd5bae2dca2e5e2ab2f2c00669
Tim Perry <pimterry@gmail.com>
2024-04-02 19:35:43 +0200

    Add HTTP error handling & improved path matching

--- src/http-handler.ts
+++ src/http-handler.ts
@@ -7,33 +7,43 @@ export function createHttpHandler(options: {
     acmeChallengeCallback: (token: string) => string | undefined
 }) {
     const handler = new http.Server(async (req, res) => {
-        console.log(`Handling request to ${req.url}`);
+        try {
+            console.log(`Handling request to ${req.url}`);
 
-        if (req.url?.startsWith('/.well-known/acme-challenge/')) {
-            const token = req.url.split('/')[3];
-            const response = options.acmeChallengeCallback(token);
-            if (response) {
-                res.writeHead(200);
-                res.end(response);
+            const url = new URL(req.url!, `http://${req.headers.host}`);
+            const path = url.pathname;
+
+            if (path.startsWith('/.well-known/acme-challenge/')) {
+                const token = path.split('/')[3];
+                const response = options.acmeChallengeCallback(token);
+                if (response) {
+                    res.writeHead(200);
+                    res.end(response);
+                } else {
+                    res.writeHead(404);
+                    res.end('Unrecognized ACME challenge request');
+                }
+            } else if (path === '/echo') {
+                await streamConsumers.buffer(req); // Wait for all request data
+                const input = Buffer.concat(req.socket.receivedData ?? []);
+                res.writeHead(200, {
+                    'Content-Length': Buffer.byteLength(input)
+                });
+                res.end(input);
             } else {
                 res.writeHead(404);
-                res.end('Unrecognized ACME challenge request');
+                res.end(`No handler for ${req.url}`);
             }
-        } else if (req.url === '/echo') {
-            await streamConsumers.buffer(req); // Wait for all request data
-            const input = Buffer.concat(req.socket.receivedData ?? []);
-            res.writeHead(200, {
-                'Content-Length': Buffer.byteLength(input)
-            });
-            res.end(input);
-        } else {
-            res.writeHead(404);
-            res.end(`No handler for ${req.url}`);
-        }
 
-        // We have to clear this, as we might get multiple requests on the same
-        // socket with keep-alive etc.
-        clearArray(req.socket.receivedData);
+            // We have to clear this, as we might get multiple requests on the same
+            // socket with keep-alive etc.
+            clearArray(req.socket.receivedData);
+        } catch (e) {
+            console.error(e);
+
+            res.writeHead(500);
+            res.end('HTTP handler failed');
+        }
     });
 
     handler.on('error', (err) => console.error('HTTP handler error', err));
