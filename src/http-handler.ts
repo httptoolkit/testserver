b1a5434e0560a47ce9e62eed9e1b0336eb840db3
Tim Perry <pimterry@gmail.com>
2024-04-03 12:01:26 +0200

    Refactor HTTP handler to clarify error/cleanup handling

--- src/http-handler.ts
+++ src/http-handler.ts
@@ -29,102 +29,109 @@ const allowCORS = (req: http.IncomingMessage, res: http.ServerResponse) => {
 export function createHttpHandler(options: {
     acmeChallengeCallback: (token: string) => string | undefined
 }) {
-    const handler = new http.Server(async (req, res) => {
-        try {
-            console.log(`Handling request to ${req.url}`);
+    async function handleRequest(req: http.IncomingMessage, res: http.ServerResponse) {
+        const url = new URL(req.url!, `http://${req.headers.host}`);
+        const path = url.pathname;
 
-            const url = new URL(req.url!, `http://${req.headers.host}`);
-            const path = url.pathname;
-
-            if (path.startsWith('/.well-known/acme-challenge/')) {
-                const token = path.split('/')[3];
-                const response = options.acmeChallengeCallback(token);
-                if (response) {
-                    res.writeHead(200);
-                    res.end(response);
-                } else {
-                    res.writeHead(404);
-                    res.end('Unrecognized ACME challenge request');
-                }
-
-                // We have to clear this, as we might get multiple requests on the same
-                // socket with keep-alive etc.
-                clearArray(req.socket.receivedData);
-                return;
-            }
-
-            // Now we begin the various test endpoints themselves:
-            allowCORS(req, res);
-            if (req.method === 'OPTIONS') {
-                // Handle preflight CORS requests for everything
+        if (path.startsWith('/.well-known/acme-challenge/')) {
+            const token = path.split('/')[3];
+            const response = options.acmeChallengeCallback(token);
+            if (response) {
                 res.writeHead(200);
-                res.end();
-            } else if (path === '/echo') {
-                await streamConsumers.buffer(req); // Wait for all request data
-                const input = Buffer.concat(req.socket.receivedData ?? []);
-                res.writeHead(200, {
-                    'Content-Length': Buffer.byteLength(input)
-                });
-                res.end(input);
-            }
-
-            // --- Now we start on the httpbin.org endpoints ---
-            else if (path.match(/^\/anything(\/|$)/)) {
-                await anythingEndpoint(req, res);
-            } else if (http.METHODS.includes(path.slice(1))) {
-                const method = path.slice(1);
-                await anythingEndpoint(req, res, {
-                    requiredMethod: method,
-                    fieldFilter: method === 'GET'
-                        ? ["url", "args", "headers", "origin"]
-                        : ["url", "args", "form", "data", "origin", "headers", "files", "json"]
-                });
-            } else if (path.startsWith('/status/')) {
-                const statusCode = parseInt(path.slice('/status/'.length), 10);
-                if (isNaN(statusCode)) {
-                    res.writeHead(400);
-                    res.end('Invalid status code');
-                } else {
-                    res.writeHead(statusCode);
-                    res.end();
-                }
-            } else if (path === '/headers') {
-                return anythingEndpoint(req, res, { fieldFilter: ["headers"] });
-            } else if (path === '/ip') {
-                return anythingEndpoint(req, res, { fieldFilter: ["origin"] });
-            } else if (path === '/user-agent') {
-                res.writeHead(200);
-                res.end('');
-            } else if (path.startsWith('/delay/')) {
-                const delayMs = parseFloat(path.slice('/delay/'.length));
-
-                if (isNaN(delayMs)) {
-                    res.writeHead(400);
-                    res.end('Invalid delay duration');
-                }
-
-                await delay(Math.min(delayMs, 10 * 1000)); // 10s max
-
-                return anythingEndpoint(req, res, {
-                    fieldFilter: ["url", "args", "form", "data", "origin", "headers", "files"]
-                });
-
-            }
-            // --- Last httpbin org endpoint ---
-
-            else {
+                res.end(response);
+            } else {
                 res.writeHead(404);
-                res.end(`No handler for ${req.url}`);
+                res.end('Unrecognized ACME challenge request');
             }
 
             // We have to clear this, as we might get multiple requests on the same
             // socket with keep-alive etc.
             clearArray(req.socket.receivedData);
+            return;
+        }
+
+        // Now we begin the various test endpoints themselves:
+        allowCORS(req, res);
+        if (req.method === 'OPTIONS') {
+            // Handle preflight CORS requests for everything
+            res.writeHead(200);
+            res.end();
+        } else if (path === '/echo') {
+            await streamConsumers.buffer(req); // Wait for all request data
+            const input = Buffer.concat(req.socket.receivedData ?? []);
+            res.writeHead(200, {
+                'Content-Length': Buffer.byteLength(input)
+            });
+            res.end(input);
+        }
+
+        // --- Now we start on the httpbin.org endpoints ---
+        else if (path.match(/^\/anything(\/|$)/)) {
+            await anythingEndpoint(req, res);
+        } else if (http.METHODS.includes(path.slice(1))) {
+            const method = path.slice(1);
+            await anythingEndpoint(req, res, {
+                requiredMethod: method,
+                fieldFilter: method === 'GET'
+                    ? ["url", "args", "headers", "origin"]
+                    : ["url", "args", "form", "data", "origin", "headers", "files", "json"]
+            });
+        } else if (path.startsWith('/status/')) {
+            const statusCode = parseInt(path.slice('/status/'.length), 10);
+            if (isNaN(statusCode)) {
+                res.writeHead(400);
+                res.end('Invalid status code');
+            } else {
+                res.writeHead(statusCode);
+                res.end();
+            }
+        } else if (path === '/headers') {
+            return anythingEndpoint(req, res, { fieldFilter: ["headers"] });
+        } else if (path === '/ip') {
+            return anythingEndpoint(req, res, { fieldFilter: ["origin"] });
+        } else if (path === '/user-agent') {
+            res.writeHead(200);
+            res.end('');
+        } else if (path.startsWith('/delay/')) {
+            const delayMs = parseFloat(path.slice('/delay/'.length));
+
+            if (isNaN(delayMs)) {
+                res.writeHead(400);
+                res.end('Invalid delay duration');
+            }
+
+            await delay(Math.min(delayMs, 10 * 1000)); // 10s max
+
+            return anythingEndpoint(req, res, {
+                fieldFilter: ["url", "args", "form", "data", "origin", "headers", "files"]
+            });
+        }
+        // --- Last httpbin org endpoint ---
+
+        else {
+            res.writeHead(404);
+            res.end(`No handler for ${req.url}`);
+        }
+    }
+
+    const handler = new http.Server(async (req, res) => {
+        try {
+            console.log(`Handling request to ${req.url}`);
+            await handleRequest(req, res);
         } catch (e) {
             console.error(e);
 
-            res.writeHead(500);
-            res.end('HTTP handler failed');
+            if (res.closed) return;
+            else if (res.headersSent) {
+                res.destroy();
+            } else {
+                res.writeHead(500);
+                res.end('HTTP handler failed');
+            }
+        } finally {
+            // We have to clear this, as we might get multiple requests on the same
+            // socket with keep-alive etc.
+            clearArray(req.socket.receivedData);
         }
     });
 
